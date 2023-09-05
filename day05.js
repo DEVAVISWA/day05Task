654f284ab39644e4ddc3b439e2ffb2e77826aaba
DEVAVISWA G <140805126+DEVAVISWA@users.noreply.github.com>
2023-09-06 00:36:12 +0530

    Add files via upload

---
+++ day05.js
@@ -0,0 +1,107 @@
+//1) forloop for given json
+
+// const countryObj = {
+//         country1 : "india" ,
+//         country2 : "china",
+//         country3 : "japan",
+//         country4 : "usa",
+//         country5 : "uk",
+//         country6 : "europe",
+    
+//     }
+// //     //keys
+//     const countryobjectKeys= Object.keys(countryObj)
+//     for(i=0;i<countryobjectKeys.length;i++) {
+//         console.log(countryobjectKeys[i])
+//     }
+//     console.log(countryobjectKeys)
+// //     //values
+//     const countryObjValues = Object.values(countryObj)
+//     for(i=0;i<countryObjValues.length;i++) {
+//         console.log(countryObjValues[i])
+//     }
+//     console.log(countryObjValues)
+
+// 1) foreach for given json
+// const countryObj = {
+//         country1 : "india" ,
+//         country2 : "china",
+//         country3 : "japan",
+//         country4 : "usa",
+//         country5 : "uk",
+//         country6 : "europe",
+    
+//     }
+
+// let keys = Object.keys(countryObj)
+
+// keys.forEach((key,index,fullArray) => {
+//     console.log(`
+//     key:${key}
+//     value:${countryObj[key]}
+//     index:${index}
+//     fullArray:${fullArray}
+
+//     `);
+// })
+
+//1) for of forTheJSON
+// const countryObj = {
+//     country1 : "india" ,
+//     country2 : "china",
+//     country3 : "japan",
+//     country4 : "usa",
+//     country5 : "uk",
+//     country6 : "europe",
+
+// }
+// let keys = Object.keys(countryObj)
+// for(abc of keys) {
+//     console.log(`
+//     keys:${keys}
+//     value:${countryObj[abc]}`)
+// }
+
+//1) for in loop forTheJSON
+// const countryObj = {
+//         country1 : "india" ,
+//         country2 : "china",
+//         country3 : "japan",
+//         country4 : "usa",
+//         country5 : "uk",
+//         country6 : "europe",
+    
+//     }
+//     for(abc in countryObj) {
+//         console.log(`
+//         key  :${abc}
+//         value:${countryObj[abc]}
+//         `)
+//     }
+
+//RESUME IN JSON
+
+// const myResume = {
+//     Personal:{
+//         name: "Devaviswa G",
+//         mobileNumber: 9876543219,
+//         mailID: "devaviswa15@gmail.com",
+//         DOB: "16-July-1998"
+//     },
+//         Education:{
+//         Degree: "BE MECH" ,
+//         CGPA: 7.01 ,
+//         PassedOutYear: 2019,
+//         CollegeName: "saranathan colege of engneering"
+//     },
+//     WorkExperience:{
+//         CompanyName:"Propel Industries Pvt Ltd",
+//         Year : "2019-2020" ,
+//         jobRole: "Graduate Engineer Trainee",
+//     },
+//     SkillsSet : {
+//         Certification: "AutoCADD and PTC Creo",
+//         InterPersonalSkills :"efficient worker ,good communication,creative,decision making"
+//     },
+//     DateOfJoining : "Immediate",
+// }
